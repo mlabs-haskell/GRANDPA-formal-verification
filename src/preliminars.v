a6a5e79bde2a78d8e5794bd93717331230223469
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-03-06 00:56:15 -0600

    refactors after the answer to 2.5 lemma

--- src/preliminars.v
+++ src/preliminars.v
@@ -41,10 +41,10 @@ Inductive Block : nat -> Type:=
 *)
 Inductive Prefix : forall {n}, Block n -> forall {m}, Block m -> Type :=
   (* C <= C*)
-  | Same : forall n (B : Block n), Prefix B B
+  | Same : forall n (block : Block n), Prefix block block
   (* C <= C'  ->  C <= (B :: C')*)
-  | IsPrefix {n m} (B: Block n) (B': Block m) (b : nat) :
-      Prefix B B' -> Prefix B (NewBlock B b).
+  | IsPrefix {n m} (block1: Block n) (block2: Block m) (block_id : nat) :
+      Prefix block1 block2 -> Prefix block1 (NewBlock block2 block_id).
 
 
 (* 
@@ -54,18 +54,81 @@ Inductive Prefix : forall {n}, Block n -> forall {m}, Block m -> Type :=
 
  *)
 Inductive Related : forall {n}, Block n -> forall {m}, Block m -> Type :=
-  | IsLower {n m } (B:Block n) (B':Block m) : Prefix B B' -> Related B B'
-  | IsUpper {n m } (B:Block n) (B':Block m)  : Prefix B' B -> Related B' B.
+  | IsLower {n m } (block1:Block n) (block2:Block m) 
+    : Prefix block1 block2 -> Related block1 block2
+  | IsUpper {n m } (block1:Block n) (block2:Block m)  
+    : Prefix block2 block1 -> Related block2 block1.
 
 
-Inductive Voter : Type := |VoterC (n:nat) :Voter.
+Definition Voter : Type := nat.
 
-Inductive Vote {n} : Block n -> Type := 
-  | VoteC {m} {original_chain: Block n} (block: Block m) (is_prefix: Prefix original_chain block) (voter : Voter) : Vote original_chain.
+Definition Voters := list Voter.
 
-Inductive Votes {n} : Block n ->  Type := | VotesC (B : Block n) (l:list (Vote B)): Votes B.
+Inductive Vote {last_block_number} 
+  :Voters -> Block last_block_number -> Type 
+  := 
+    | VoteC {m}  (voters : Voters) (voter : Voter) 
+      (is_voter: List.In voter voters ) 
+      (original_chain: Block last_block_number) 
+      (block: Block m) (is_prefix: Prefix original_chain block)
+      : Vote voters original_chain.
 
 
+Inductive Votes  {last_block_number}
+  : Voters -> Block last_block_number ->  Type 
+  := 
+    | VotesC (voters:Voters ) {B : Block last_block_number}
+      (votes_list:list (Vote voters B))
+      : Votes voters B.
+
+
+Definition has_supermajority  {last_block_number}
+  (voters:Voters)
+  (last_block : Block last_block_number)
+  (S : Votes voters last_block) 
+  (f : nat) 
+  (f_is_low : (3 * f < length voters))
+  : Type.
+(* TODO: Provide Definition *)
+Admitted.
+
+(*
+  let n := length voters in
+  let threshold := (n + f + 1) / 2 in
+  let valid_votes := 
+    filter (fun v => match v with 
+                     | VoteC _ _ _ _ _ _ => true 
+                     end) 
+           voters in
+  let valid_votes_count := length valid_votes in
+  let equivocating_voters := 
+    voters_upper - length (remove_duplicates Nat.eq_dec 
+                                (map (fun v => projT2 v) valid_votes)) in
+  valid_votes_count >= threshold \/ equivocating_voters >= f.
+*)
+
+(* Función g *)
+Definition g {last_block_number voters_upper} 
+             (T : Votes (Voters ) (Block last_block_number)) 
+           : Block last_block_number.
+(* TODO: Provide definition *)
+Admitted.
+
+(*
+  let valid_votes := 
+    filter (fun v => match v with 
+                     | VoteC _ _ _ _ _ _ => true 
+                     | _ => false 
+                     end) 
+           (projT2 T) in
+  let sorted_votes := 
+    sort (fun v1 v2 => if leb (projT2 v1) (projT2 v2) then true else false) 
+         valid_votes in
+  match sorted_votes with
+  | [] => OriginBlock
+  | h :: _ => projT1 h
+  end.
+*)
 
 (* TODO: Define:
 
@@ -121,5 +184,5 @@ Inductive Round : Voters-> Time -> nat -> Type :=
 
 
  *)
-End preliminars.
+End BlockChain.
 
