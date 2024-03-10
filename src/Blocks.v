fcbfadce620214453eff53fa7b325efd485a7469
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-03-09 18:36:00 -0600

    updated documentation for Blocks.

--- src/Blocks.v
+++ src/Blocks.v
@@ -1,48 +1,42 @@
-Require Coq.Vectors.Vector.
-
-
-Definition Vector := VectorDef.t.
 (* 
-  
+
+  In the real implementation is unpractical to have the full
+  history of the blockchain and instead a new block stores
+  the hash of the last block. In our case storing the hash
+  only would lead to complications, as so, we choose to 
+  store the full chain of blocks.
+
+  Blocks are indexed by it's index in the chain.
+
+  Originally we used the Vector type in the Coq prelude, 
+  but it complicates the requirement of the `Original` block.
 *)
 
 Inductive Block : nat -> Type:= 
   | OriginBlock : Block 0
+  (* 
+     The id only purpose is to bring us the ability to talk about 
+   different blocks.
+   *)
   | NewBlock {n} (oldBlock : Block n) (id:nat) : Block (S n).
 
 
 
-(* 
-  In the paper they constantly use B < B' as blocks, using the fact
-   that a block contains the hash of it's predecessor. To facilitate 
-   our lives, we replaced the hash of the previous block with the 
-   entire Block-Chain. This isn't wise in a real implementation,
-   but would simplify our proofs.
-   This means that whenever the paper compares two blocks we would 
-   replace the statement with a equivalent one comparing two 
-   Chains.
-   Additionally we are choosing Vectors to represent a chain of blocks 
-   since we are only interested in two things: 
-   - Block number (provided by the index of the block in the chain)
-   - A way to have different blocks.
- *)
 
 (* 
-  This corresponds to head(C) <= head(C').
-  There exits various ways to define this relation but we believe 
-  that this one would be quite useful.
+  ` Prefix B B' ` in the paper is presented as ` B <=B' `
 *)
 Inductive Prefix : forall {n}, Block n -> forall {m}, Block m -> Type :=
-  (* C <= C*)
+  (* B <= B *)
   | Same : forall n (block : Block n), Prefix block block
-  (* C <= C'  ->  C <= (B :: C')*)
+  (* B <= B'  ->  B <= (b :: B') *)
   | IsPrefix {n m} (block1: Block n) (block2: Block m) (block_id : nat) :
       Prefix block1 block2 -> Prefix block1 (NewBlock block2 block_id).
 
 
 (* 
    Is equivalent to the relation B ~ B' in the paper
-   We express it like `(B <= B' ) /\ (B' <= B)` instead 
+   We express it like `(B <= B' ) \/ (B' <= B)` instead 
    of the one in the paper `B<B' /\ B=B! /\ B>B'` .
 
  *)
