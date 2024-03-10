eefe4c0ff41f866af80899ca58b628351bd62ef0
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-03-09 18:11:19 -0600

    lemmas for section 2 are expressed.

--- src/preliminars.v
+++ src/preliminars.v
@@ -2,6 +2,8 @@
 Require Coq.Vectors.Vector.
 
 Require List.
+Require Nat.
+Require Coq.Init.Nat.
 
 Definition Vector := VectorDef.t.
 
@@ -53,42 +55,102 @@ Inductive Prefix : forall {n}, Block n -> forall {m}, Block m -> Type :=
    of the one in the paper `B<B' /\ B=B! /\ B>B'` .
 
  *)
-Inductive Related : forall {n}, Block n -> forall {m}, Block m -> Type :=
+Variant Related : forall {n}, Block n -> forall {m}, Block m -> Prop :=
   | IsLower {n m } (block1:Block n) (block2:Block m) 
     : Prefix block1 block2 -> Related block1 block2
   | IsUpper {n m } (block1:Block n) (block2:Block m)  
     : Prefix block2 block1 -> Related block2 block1.
 
+Definition Unrelated {n m} (block1 : Block n) (block2 :Block m) : Type := not (Related block1 block2).
+
 
 Definition Voter : Type := nat.
 
-Definition Voters: Type := list Voter.
-
-Inductive Vote {last_block_number} 
-  :Voters -> Block last_block_number -> Type 
+Variant Voters (bizantiners_number:nat) : Type 
   := 
-    | VoteC {m}  (voters : Voters) (voter : Voter) 
-      (is_voter: List.In voter voters ) 
+    | VotersC (voters:list Voter) 
+        (*TODO: add a constraint here to make the voters to only appear 
+        once, or use a proper set.*)
+        (bizantiners_are_lower: 3*bizantiners_number < length voters) 
+      : Voters  bizantiners_number .
+
+
+Definition inVoters {bizantiners_number} 
+  (voter : Voter) (voters:Voters bizantiners_number) 
+  :=
+  match voters with 
+  | VotersC _ l _ => List.In voter l
+  end.
+
+Definition votersLength {bizantiners_number} (voters:Voters bizantiners_number)
+  := 
+  match voters with
+  | VotersC _ l _ => length l
+  end.
+
+Inductive Vote {bizantiners_number last_block_number}
+  :Voters bizantiners_number -> Block last_block_number -> Type 
+  := 
+    | VoteC {m}  (voters : Voters bizantiners_number) (voter : Voter) 
+      (is_voter: inVoters voter voters ) 
       (original_chain: Block last_block_number) 
       (block: Block m) (is_prefix: Prefix original_chain block)
       : Vote voters original_chain.
 
 
-Inductive Votes  {last_block_number}
-  : Voters -> Block last_block_number ->  Type 
+Inductive Votes  {bizantiners_number last_block_number}
+  (voters: Voters bizantiners_number) (last_block:Block last_block_number)
+  :Type 
   := 
-    | VotesC (voters:Voters ) {B : Block last_block_number}
-      (votes_list:list (Vote voters B))
-      : Votes voters B.
+    | VotesC
+      (votes_list:list (Vote voters last_block))
+      : Votes voters last_block.
 
 
-Definition has_supermajority  {last_block_number}
-  (voters:Voters)
-  (last_block : Block last_block_number)
+Definition Equivocate {bizantiners_number last_block_number } 
+  {voters: Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (voter: Voter)
+  (votes: Votes voters last_block)
+  : bool
+  :=
+    let unvote vote1 := match  vote1 with
+      |VoteC  _  voter _ _ _ _ => voter
+      end
+    in
+    match votes with 
+    | VotesC  _ _ votes_list => 
+      let voters_list : list Voter := List.map unvote votes_list
+      in
+      let has_same_voter (new_vote : Voter) := Nat.eqb new_vote voter 
+      in
+      let filtered := List.filter has_same_voter voters_list
+      in
+        Nat.ltb (length filtered) 1
+    end.
+
+Fixpoint isSafe {bizantiners_number last_block_number } 
+  {voters: Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (votes: Votes voters last_block)
+  :bool.
+  Admitted.
+  (*
+    match voters with 
+    | VotersC  _ voters_list _ => 
+        match voters_list with
+        | nil => true
+        | List.cons x y => andb (negb (Equivocate x votes) ) (isSafe (Votes voters y))
+        end
+     end.
+  *)
+
+
+Definition has_supermajority  {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
   (S : Votes voters last_block) 
-  (f : nat) 
-  (f_is_low : (3 * f < length voters))
-  : Type.
+  : bool.
 (* TODO: Provide Definition *)
 Admitted.
 
@@ -107,16 +169,13 @@ Admitted.
   valid_votes_count >= threshold \/ equivocating_voters >= f.
 *)
 
-Inductive Maybe : Type -> Type :=
- | Just  {T:Type}  (t:T) : Maybe T
- | Nothing  {T:Type}: Maybe T.
 
 (* Función g *)
-Definition g {last_block_number} 
-  (voters:Voters)
-  (last_block : Block last_block_number)
+Definition g {bizantiners_number last_block_number out}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
   (T : Votes voters last_block) 
-  : Maybe (Block last_block_number).
+  : option (Block out).
 (* TODO: Provide definition *)
 Admitted.
 
@@ -136,13 +195,93 @@ Admitted.
   end.
 *)
 
+Definition IsSubset {bizantiners_number last_block_number_s last_block_number_t} 
+  {voters:Voters bizantiners_number}
+  {last_block_s : Block last_block_number_s}
+  {last_block_t : Block last_block_number_t}
+  (S : Votes voters last_block_s)
+  (T : Votes voters last_block_t) : Prop.
+Admitted.
+
+Lemma lemma_2_5_2 {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (T: Votes voters last_block)
+  (is_safe_t: isSafe T = true)
+  (S: Votes voters last_block) 
+  (is_sub_set: IsSubset S T )
+  {gs_block_number: nat}
+  (gs : Block gs_block_number)
+  (gs_is_result : g S = Some gs)
+  (gt_block_number: nat)
+  (gt : Block gt_block_number)
+  (gt_is_result : g T = Some gt)
+  :Prefix gs gt. 
+Admitted.
+
+
+Variant ImpossibleSupermajority {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (T: Votes voters last_block)
+  {new_block_number: nat}
+  (new_block: Block new_block_number)
+  :Prop
+  := 
+  | ImpossibleByOtherChain {other_block_number:nat} (other_block: Block other_block_number) (*TODO: fill this*) : ImpossibleSupermajority T new_block
+  | ImpossibleByEquivocations (*TODO FIll this*) : ImpossibleSupermajority T new_block.
+
+
+Definition PossibleSupermajority {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (T: Votes voters last_block)
+  {new_block_number: nat}
+  (new_block: Block new_block_number)
+  :Prop
+  := not (ImpossibleSupermajority T new_block).
+
+
+Lemma lemma_2_6_1 {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (S: Votes voters last_block)
+  {block1_number block2_number: nat}
+  (block1: Block block1_number)
+  (block2: Block block2_number)
+  (is_prefix: Prefix block1 block2)
+  : ImpossibleSupermajority S block1 -> ImpossibleSupermajority S block2.
+Admitted.
+
+Lemma lemma_2_6_2 {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (S T: Votes voters last_block)
+  {block_number : nat}
+  (block: Block block_number)
+  : ImpossibleSupermajority S block -> ImpossibleSupermajority T block.
+Admitted.
+
+Lemma lemma_2_6_3 {bizantiners_number last_block_number}
+  {voters:Voters bizantiners_number}
+  {last_block : Block last_block_number}
+  (S: Votes voters last_block)
+  {gs_block_number:nat}
+  (gs : Block gs_block_number)
+  (gs_is_result : g S = Some gs)
+  {block_number : nat}
+  (block: Block block_number)
+  (unrelated_proof: Unrelated block gs)
+  : ImpossibleSupermajority S block.
+Admitted.
+
+  
+
+
+
 (* TODO: Define:
 
 - Time
-- Voter 
-- Set of Votes 
-- SuperMajority
-- g
 - Round
 - Estimate 
 
