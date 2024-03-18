0140f05313f02049330d040ec97e0431e16feb97
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-03-18 03:30:04 -0600

    added completable

--- src/Protocol.v
+++ src/Protocol.v
@@ -62,7 +62,8 @@ Variant Estimate {preview_number precommit_number : nat}
   {last_block_number} 
   {last_block : Block last_block_number} 
   (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
-  : Type
+  (*TODO Make Estimate part of Type instead of Prop *)
+  : nat -> Prop
   :=
   (*TODO: add origin block case*)
     |EstimateC 
@@ -72,8 +73,23 @@ Variant Estimate {preview_number precommit_number : nat}
     {g_block_number: nat}
     (g_preview: Block g_block_number)
     (g_preview_is_some : g ( get_preview_votes round_state) = Some g_preview)
-    (new_block_is_children_of_g: IsChildren  new_block g_preview)
-    : Estimate round_state.
+    (new_block_is_ancestor_of_g: Prefix new_block g_preview)
+    : Estimate round_state new_block_number.
+
+
+Definition get_estimate_block
+  {preview_number precommit_number : nat}
+  {preview_voters : Voters preview_number} 
+  {precommit_voters: Voters precommit_number}
+  {round_time:Time}
+  {round_number:nat}
+  {last_block_number} 
+  {last_block : Block last_block_number} 
+  {round_state: RoundState preview_voters precommit_voters  round_time last_block round_number}
+  {n}
+  (e:Estimate round_state n)
+  : Block n.
+Admitted.
 
 Definition get_estimate {preview_number precommit_number : nat}
   {preview_voters : Voters preview_number} 
@@ -83,7 +99,30 @@ Definition get_estimate {preview_number precommit_number : nat}
   {last_block_number} 
   {last_block : Block last_block_number} 
   (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
-  : option (Estimate  round_state).
+  : option (exists n, Estimate  round_state n).
 Admitted.
 
+
+Variant Completable {preview_number precommit_number : nat}
+  {preview_voters : Voters preview_number} 
+  {precommit_voters: Voters precommit_number}
+  {round_time:Time}
+  {round_number:nat}
+  {last_block_number} 
+  {last_block : Block last_block_number} 
+  (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
+  : Type
+  :=
+  | CompletableBelowPreview {estimate_block_number}
+      (e: Estimate  round_state estimate_block_number)
+      {g_block_number: nat}
+      (g_preview: Block g_block_number)
+      (g_preview_is_some : g ( get_preview_votes round_state) = Some g_preview)
+      (new_block_is_below_g: estimate_block_number < g_block_number)
+  | CompletableByImpossible 
+      {g_block_number: nat}
+      (g_preview: Block g_block_number)
+      (g_preview_is_some : g ( get_preview_votes round_state) = Some g_preview)
+      (cant_have_supermajority : forall n (block : Block n) , g_block_number < n -> hasSupermajority (get_precommit_votes round_state) = false)
+  .
 
