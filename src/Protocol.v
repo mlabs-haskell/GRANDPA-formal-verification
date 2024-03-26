ac48371345e76886a181398bf2cefd8b8ca45549
Luis Alberto Díaz Díaz <73986926+Luis-omega@users.noreply.github.com>
2024-03-26 17:35:50 -0600

    save of proof for get_stimate

--- src/Protocol.v
+++ src/Protocol.v
@@ -50,8 +50,12 @@ Definition get_preview_votes
 Definition get_precommit_votes
   (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
   : 
-  (Votes precommit_voters last_block).
-Admitted.
+  (Votes precommit_voters last_block)
+  :=
+  match round_state with
+  | InitialRoundState _ _ _ _ => VotesC _ _ List.nil (* No votes in initial round state *)
+  | RoundStateUpdate _ _ _ _ _ _ _ new_precommit_votes => new_precommit_votes
+  end.
 
 End State1.
 
@@ -63,9 +67,9 @@ Variant Estimate {preview_number precommit_number : nat}
   {last_block_number} 
   {last_block : Block last_block_number} 
   (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
-  : nat -> Type
+  : {n & Block n} -> Type
   :=
-  (*TODO: add origin block case*)
+    | EstimateOrigin : round_number = 0 -> Estimate round_state (existT _ 0 OriginBlock)
     |EstimateC 
     {new_block_number : nat}
     (new_block : Block new_block_number)
@@ -74,7 +78,7 @@ Variant Estimate {preview_number precommit_number : nat}
     (g_preview: Block g_block_number)
     (g_preview_is_some : g ( get_preview_votes round_state) = Some (existT _ g_block_number g_preview))
     (new_block_is_ancestor_of_g: Prefix new_block g_preview)
-    : Estimate round_state new_block_number.
+    : Estimate round_state (existT _ new_block_number new_block).
 
 
 
@@ -91,14 +95,83 @@ Context {round_number: nat}.
 Definition get_estimate_block
   {round_state: RoundState preview_voters precommit_voters  round_time last_block round_number}
   {n}
-  (e:Estimate round_state n)
-  : Block n.
-Admitted.
+  {block : Block n}
+  (estimate :Estimate round_state (existT _ n block))
+  : Block n
+  :=
+  match estimate with
+    | EstimateOrigin _ _ => OriginBlock
+    | EstimateC _ new_block _ _ _ _ => new_block
+  end.
+
+Print get_preview_votes.
+
+Definition get_round_number
+  (round_number2:Time)
+  (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number2)
+  : Type
+  :=
+    match round_state with 
+    | InitialRoundState _ _ _ _ => RoundState preview_voters precommit_voters round_time last_block 0
+    | _ => RoundState preview_voters precommit_voters round_time last_block round_number2
+    end.
+
+Definition get_estimate_case_1
+  (round_state: RoundState preview_voters precommit_voters  round_time last_block 0)
+  : option (Estimate round_state (existT _ 0 OriginBlock))
+  :=
+    match round_state with 
+    | InitialRoundState _ _ _ _ => Some (EstimateOrigin round_state eq_refl)
+    | _ => None
+    end.
+
+Require Import Equality.
+Print block.
+Locate block.
+
+Definition get_estimate (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
+  : option {n & { block & Estimate round_state (existT _ n block)}}
+  :=
+ match round_state  with
+  | InitialRoundState _ _ _ _ => 
+      Some 
+      (existT _ 0 
+        (existT _ 
+          OriginBlock 
+          (EstimateOrigin 
+            (InitialRoundState preview_voters precommit_voters round_time
+                   last_block 
+            ) 
+            eq_refl
+          )
+        )
+      )
+  | _ => None
+  end.
 
 Definition get_estimate 
   (round_state: RoundState preview_voters precommit_voters  round_time last_block round_number)
-  : option (sigT (fun n  => Estimate  round_state n)).
-Admitted.
+  : option {n & { block & Estimate round_state (existT _ n block)}}.
+Proof.
+  dependent destruction  round_state.
+  - refine (Some (existT _ 0 (existT _ OriginBlock (EstimateOrigin (InitialRoundState preview_voters precommit_voters round_time last_block) eq_refl))) ).
+  - refine None.
+Qed.
+
+
+Print get_estimate.
+  :=
+ match round_state in RoundState _ _ _ _ 0 return option {n & { block & Estimate round_state (existT _ n block)}} with
+  | InitialRoundState _ _ _ _ =>None
+      (*Some (existT _ 0 (existT _ OriginBlock (EstimateOrigin round_state _)))*)
+  | b => False_rect unit
+  end.
+  match round_number with
+  | O =>
+    Some (existT _ 0 (existT _ _ OriginBlock, EstimateOrigin eq_refl))
+  | _ => g preview_votes
+
+  end.
 
 
 Variant Completable 
